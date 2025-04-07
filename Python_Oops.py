2ee5e575889e29bc86cd18eaea6c163f7ef76230
Madhukiran00 <162177926+Madhukiran00@users.noreply.github.com>
2025-04-07 20:26:40 +0530

    Add files via upload

---
+++ Python_Oops.py
@@ -0,0 +1,28 @@
+#Object orientend programming language(Oops)
+
+#Class        =>Blue Print of the object
+#object       => instance of the class
+#Inheritance
+#Abstraction
+#Polymarphisam
+#Encapsulation
+
+#Inheritance
+#1)single
+#2)Multiple
+#3)Multilevel
+#4)Hierarchical
+#5)Hybrid
+
+#1)Single Inheritance
+
+
+
+
+
+
+
+
+
+
+
