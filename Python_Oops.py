2a728a60cfd7e79da3c9228c384df4070e37ece3
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-02 21:48:06 +0530

    python

--- Python_Oops.py
+++ Python_Oops.py
@@ -18,85 +18,85 @@
 
 #1)Single Inheritance
 
-class Parent:
-   def add(self):
-       print("This is parent class")
-class Child(Parent):
-    def sub(self):
-        print("This is child class")
-obj=Child()
-obj.add() #This is child class
-obj.sub() #This is child class
+# class Parent:
+#    def add(self):
+#        print("This is parent class")
+# class Child(Parent):
+#     def sub(self):
+#         print("This is child class")
+# obj=Child()
+# obj.add() #This is child class
+# obj.sub() #This is child class
 
 #2)Multilevel Inheritance
 
-class Parent:
-    def add(self):
-        print("This is parent class")
-class Child(Parent):
-    def sub(self):
-        print("This is child class")
-class GrandChild(Child):
-    def mul(self):
-        print("This is grand_child class")
+# class Parent:
+#     def add(self):
+#         print("This is parent class")
+# class Child(Parent):
+#     def sub(self):
+#         print("This is child class")
+# class GrandChild(Child):
+#     def mul(self):
+#         print("This is grand_child class")
 
-obj=GrandChild()
-obj.add()
-obj.sub()
-obj.mul()
+# obj=GrandChild()
+# obj.add()
+# obj.sub()
+# obj.mul()
 
 
 #3)Multiple Inheritance
-class Parent1:
-    def add(self):
-        print("This is parent1 class")
-class Parent2:
-    def sub(self):
-        print("This is parent2 class")
-class Child:
-    def mul(self):
-        print("This is child class")
+# class Parent1:
+#     def add(self):
+#         print("This is parent1 class")
+# class Parent2:
+#     def sub(self):
+#         print("This is parent2 class")
+# class Child:
+#     def mul(self):
+#         print("This is child class")
 
-obj=Child()
-obj.add()
-obj.sub() 
-obj.mul()
+# obj=Child()
+# obj.add()
+# obj.sub() 
+# obj.mul()
 
 #4)Hierarchical Inheritance
-class Parent:
-    def add(self):
-        print("This is parent class")
-class Child1(Parent):
-    def sub(self):
-        print("This is child1 class")
-class Child2(Parent):
-    def mul(self):
-        print("This is child2 class")
-obj=Child1()
-obj.add()
-obj.sub()    
-obj1=Child2()
-obj1.add()
-obj1.mul()
+# class Parent:
+#     def add(self):
+#         print("This is parent class")
+# class Child1(Parent):
+#     def sub(self):
+#         print("This is child1 class")
+# class Child2(Parent):
+#     def mul(self):
+#         print("This is child2 class")
+# obj=Child1()
+# obj.add()
+# obj.sub()    
+# obj1=Child2()
+# obj1.add()
+# obj1.mul()
 
 #5)Hybrid Inheritance 
    
-class Parent:
-    def sum(self):
-        print("This is parent class")
-class Child1(Parent):
-    def sub(self):
-        print("This is child1 class")
-class Child2(Parent):
-    def mul(self):
-        print("This is child2 class")
-class GrandChild(Child1):
-    def div(self):
-        print("This is grand child class")
-obj=GrandChild()
-obj.sum()
-obj.sub()
-obj.div()
+# class Parent:
+#     def sum(self):
+#         print("This is parent class")
+# class Child1(Parent):
+#     def sub(self):
+#         print("This is child1 class")
+# class Child2(Parent):
+#     def mul(self):
+#         print("This is child2 class")
+# class GrandChild(Child1):
+#     def div(self):
+#         print("This is grand child class")
+# obj=GrandChild()
+# obj.sum()
+# obj.sub()
+# obj.div()
 
 
 
@@ -107,27 +107,28 @@ obj.div()
 #1)Method Overloading
 #Method Overloading => Same method name with different arguments
 
-class Parent():
-    def add(self,a,b):
-        return a+b
+# class Parent():
+#     def add(self,a,b):
+#         return a+b
     
-obj=Parent()
-print(obj.add(10,20))
-print(obj.add("Madhu","Kiran"))
-print(obj.add(10.5,20.5))
+# obj=Parent()
+# print(obj.add(10,20))
+# print(obj.add("Madhu","Kiran"))
+# print(obj.add(10.5,20.5))
 
 
 #2)Method Overriding
 #Method Overriding => Same method name with same arguments
-class Parent():
-    def add(self,a,b):
-        return a+b
-class Child(Parent):
-    def add(self,a,b,c):
-        return a+b+c
-obj=Child()
-print(obj.add(10,20,30))
-print(obj.add("Madhu","Kiran","Kumar"))
+# class Parent():
+#     def add(self,a,b):
+#         return a-b
+# class Child(Parent):
+#     def add(self,a,b,c):
+#         return a+b+c
+# obj=Child()
+# print(obj.add(10,20,30))
+# print(obj.add("Madhu","Kiran","Kumar"))
+# print(obj.add(2,4,6))
 
 
 
